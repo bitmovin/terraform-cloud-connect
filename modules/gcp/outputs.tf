e19e0c30cce09a4a23e555d98264a4282079421d
Simon Tischler <simon.tischler@bitmovin.com>
2024-10-28 12:56:35 +0100

    Added more outputs

--- modules/gcp/outputs.tf
+++ modules/gcp/outputs.tf
@@ -8,6 +8,19 @@ output "network_id" {
   value = google_compute_network.bitmovin_vpc_network.name
 }
 
+output "network" {
+  value = "/global/networks/${google_compute_network.bitmovin_vpc_network.name}"
+}
+
+data "google_compute_network" "bitmovin-network" {
+  name = var.network_name
+  depends_on = [google_compute_network.bitmovin_vpc_network]
+}
+
+output "subnets" {
+  value = data.google_compute_network.bitmovin-network.subnetworks_self_links
+}
+
 output "service_account_email" {
   value = google_service_account.bitmovin_account.email
 }
